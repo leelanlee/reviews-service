48815315ae6d3bf6e2166fea9e03c9c21b5d4e8d
sband1 <sbandeali1@gmail.com>
2020-11-13 13:46:11 -0800

    Created app class component and header component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h3>What Locals Say about</h3>
+      <div>
+        <span>At least Trulia users voted on each feature.</span>
+      </div>
+    </div>
+  );
+};
+
+
+export default Header;
