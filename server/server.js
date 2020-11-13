af4e0ca9f611f43fb46338d88b1477f75aa7a565
sband1 <sbandeali1@gmail.com>
2020-11-12 20:38:03 -0800

    Implement controller functions to query database from API calls

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const controller = require('./controllers.js');
 const port = 8010;
 
+const db = require('../db/connection.js');
+
 app.use(bodyParser.json());
+app.get('/api/neighborhood/:id/stats', controller.getAllStats);
+
+app.get('/api/neighborhood/:id/reviews', controller.getAllReviews);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
