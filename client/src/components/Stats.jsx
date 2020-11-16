fb62b7512694a8ca9f09939e5f1fc33a78962a2f
sband1 <sbandeali1@gmail.com>
2020-11-15 16:18:42 -0800

    Add StatItem component to improve rendering, added showAll button

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -1,32 +1,62 @@
+/* eslint-disable max-len */
 import React from 'react';
 import './Stats.css';
 import PropTypes from 'prop-types';
+import statsDetails from '../stats';
+import StatsItem from './StatsItem';
 
-const Stats = ({ stats }) => (
-  <div className="flexbox-container">
-    <div className="flexbox-item flexbox-item-1">
-      <div className="stats-percentage">
-        <div className="thumbs-up">
-          <svg className="svg" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M9.016 11.858c3.183-.709 5.217-3.378 6.127-8.36l.245-1.349h1.37c3.81 0 6.705 3.46 6.128 7.273l-.367 2.435h3.166c2.933 0 5.04 2.83 4.284 5.653l-2.139 8.577c-.725 2.705-3.315 4.451-6.078 4.06L10.834 28.46v1.698H1.801v-18.3h7.215zm1.818 2.898v10.38l11.399 1.761c1.086.154 2.126-.547 2.417-1.632l2.139-8.577c.21-.787-.355-1.546-1.104-1.546H18.7l.285-1.888.653-4.322c.208-1.377-.506-2.645-1.62-3.2-1.198 4.73-3.597 7.805-7.184 9.024zm-3.285.387H5.086v11.73h2.463v-11.73z" fill="#869099"></path></svg>
-        </div>
-        <div className="percentage">
-          {Math.floor(stats.dog_friendly * 100)}
-          %
-        </div>
-      </div>
-      <div className="stats-category">
-        <div className="icon">
-          <svg className="svg" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M12.54 10.774a2.401 2.401 0 0 1-2.407-2.395v-.056a2.401 2.401 0 0 1 2.406-2.395 2.401 2.401 0 0 1 2.407 2.395v.056a2.401 2.401 0 0 1-2.407 2.395zm7.219 0a2.401 2.401 0 0 1-2.407-2.395v-.056a2.401 2.401 0 0 1 2.407-2.395 2.401 2.401 0 0 1 2.406 2.395v.056a2.401 2.401 0 0 1-2.406 2.395zm-.766 3.787l4.372 5.72a3.546 3.546 0 0 1-.68 4.985 3.585 3.585 0 0 1-2.164.726h-8.744a3.565 3.565 0 0 1-3.574-3.557c0-.778.257-1.535.73-2.154l4.372-5.72a3.585 3.585 0 0 1 5.688 0zm5.579-2.645a2.401 2.401 0 0 1 2.406 2.395v.056a2.401 2.401 0 0 1-2.406 2.395 2.4 2.4 0 0 1-2.407-2.395v-.056a2.4 2.4 0 0 1 2.407-2.395zm-14.439 2.451a2.401 2.401 0 0 1-2.407 2.395 2.4 2.4 0 0 1-2.406-2.395v-.056a2.4 2.4 0 0 1 2.406-2.395 2.401 2.401 0 0 1 2.407 2.395v.056z" fill="#C3D600" fillRule="evenodd"></path>
-          </svg>
-        </div>
-        <div className="stats-info">It's dog friendly</div>
-      </div>
-    </div>
-    <div className="flexbox-item flexbox-item-1"></div>
-    <div className="flexbox-item flexbox-item-1"></div>
-  </div>
+class Stats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seeAll: false,
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
 
-);
+  handleButtonClick() {
+    this.setState({
+      seeAll: !this.state.seeAll,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="flexbox-container">
+          <StatsItem percentage={this.props.stats.dog_friendly} details={statsDetails.dog_friendly} />
+          <StatsItem percentage={this.props.stats.sidewalks} details={statsDetails.sidewalks} />
+          <StatsItem percentage={this.props.stats.restaurants} details={statsDetails.restaurants} />
+        </div>
+        <div className="flexbox-container">
+          <StatsItem percentage={this.props.stats.grocery_stores} details={statsDetails.grocery_stores} />
+          <StatsItem percentage={this.props.stats.walk_night} details={statsDetails.walk_night} />
+          <StatsItem percentage={this.props.stats.streets} details={statsDetails.streets} />
+        </div>
+        {this.state.seeAll ?
+          <div>
+            <div className="flexbox-container">
+              <StatsItem percentage={this.props.stats.neighbors_friendly} details={statsDetails.neighbors_friendly} />
+              <StatsItem percentage={this.props.five_years} details={statsDetails.five_years} />
+              <StatsItem percentage={this.props.stats.holiday} details={statsDetails.holiday} />
+            </div>
+            <div className="flexbox-container">
+            <StatsItem percentage={this.props.stats.parking_easy} details={statsDetails.parking_easy} />
+            <StatsItem percentage={this.props.kids_outside} details={statsDetails.kids_outside} />
+            <StatsItem percentage={this.props.stats.quiet} details={statsDetails.quiet} />
+            </div>
+            <div className="flexbox-container">
+            <StatsItem percentage={this.props.stats.yard} details={statsDetails.yard} />
+            <StatsItem percentage={this.props.car} details={statsDetails.car} />
+            <StatsItem percentage={this.props.stats.wildlife} details={statsDetails.wildlife} />
+            </div>
+          </div>
+        : null}
+        <button className="stats-btn" type="button" onClick={this.handleButtonClick}>See All</button>
+      </div>
+    );
+  }
+};
 
 Stats.propTypes = {
   stats: PropTypes.object,
