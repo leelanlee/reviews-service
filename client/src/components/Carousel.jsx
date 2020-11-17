258e6ebaa305c1fe9f996779aa6bc9a7a1111517
sband1 <sbandeali1@gmail.com>
2020-11-16 22:58:06 -0800

    Implement carousel try 1

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -10,7 +10,9 @@ class Carousel extends React.Component {
     super(props);
     this.state = {
       reviews: [],
-    }
+    };
+    this.handleRightButtonClick = this.handleRightButtonClick.bind(this);
+    this.handleLeftButtonClick = this.handleLeftButtonClick.bind(this);
   }
 
   componentDidMount() {
@@ -27,14 +29,42 @@ class Carousel extends React.Component {
       .catch((err) => console.log(err));
   }
 
+  handleRightButtonClick() {
+    const container = document.querySelector('.carousel-container');
+    const rowSet = Array.from(container.children);
+    const rowWidth = rowSet[0].getBoundingClientRect().width;
+    rowSet[0].style.left = 0 + 'px';
+    rowSet[1].style.left =  rowWidth + 'px';
+
+    const currentRow = rowSet[0];
+    const nextRow = rowSet[1];
+
+    const amountToMove = nextRow.style.left;
+    container.style.transform = 'translateX(-' + amountToMove + ')';
+  };
+
+  handleLeftButtonClick() {
+    const container = document.querySelector('.carousel-container');
+    const rowSet = Array.from(container.children);
+    const rowWidth = rowSet[0].getBoundingClientRect().width;
+    rowSet[1].style.left = 0 + 'px';
+    rowSet[0].style.left =  rowWidth + 'px';
+
+    const currentRow = rowSet[1];
+    const nextRow = rowSet[0];
+
+    const amountToMove = nextRow.style.left;
+    container.style.transform = 'translateX(' + '2px' + ')';
+  };
+
   render() {
     return (
       <div className="carousel">
-        <button className="carousel-btn carousel-btn-left">
+        <button className="carousel-btn carousel-btn-left" onClick={this.handleLeftButtonClick}>
           <img src="https://www.pngfind.com/pngs/m/141-1415532_png-file-svg-carousel-button-left-right-transparent.png" />
         </button>
-        <div className="carousel-track-container">
-          <div className="flexbox-container-carousel row">
+        <div className="carousel-container">
+          <div className="flexbox-container-carousel row current-row">
             <CarouselItem />
             <CarouselItem />
             <CarouselItem />
@@ -47,8 +77,8 @@ class Carousel extends React.Component {
             <CarouselItem />
           </div>
         </div>
-        <button className="carousel-btn carousel-btn-left">
-          <img src="https://www.pngfind.com/pngs/m/141-1415532_png-file-svg-carousel-button-left-right-transparent.png" />
+        <button className="carousel-btn carousel-btn-right">
+          <img src="https://www.pngfind.com/pngs/m/141-1415532_png-file-svg-carousel-button-left-right-transparent.png" onClick={this.handleRightButtonClick}/>
         </button>
       </div>
     );
