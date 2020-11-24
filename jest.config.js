88b880f4cccade5089ee9cba2e8e8624ffae4885
sband1 <sbandeali1@gmail.com>
2020-11-24 13:27:28 -0800

    Added jest coverage reports

--- jest.config.js
+++ jest.config.js
@@ -21,20 +21,20 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which
   // coverage information should be collected
-  collectCoverageFrom: [
-    "/specs/*.test*"
-  ],
+  // collectCoverageFrom: [
+  //   'specs/App.test.jsx',
+  // ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
-    '/node_modules/'
+    '/node_modules/',
   ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  // coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
